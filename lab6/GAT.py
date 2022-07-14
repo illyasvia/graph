979cd2dbb01e28d9212cdfee9df23ff54ac0493d
illyasvia <88422980+illyasvia@users.noreply.github.com>
2022-07-14 16:24:00 +0800

    GAT

---
+++ lab6/GAT.py
@@ -0,0 +1,53 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class GraphAttentionLayer(nn.Module):
+
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+        super(GraphAttentionLayer, self).__init__()
+        self.dropout = dropout
+        self.in_features = in_features
+        self.out_features = out_features
+        self.alpha = alpha
+        self.concat = concat
+
+        self.weight = nn.Parameter(torch.zeros(size=(in_features, out_features)))
+        nn.init.xavier_uniform_(self.weight.data, gain=1.414)
+        self.a = nn.Parameter(torch.zeros(size=(2 * out_features, 1)))
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+    def forward(self, input, adj):
+        # input为输入特征向量
+        h = torch.mm(input, self.weight)  # 矩阵乘法
+        N = h.size()[0]
+        a_input = torch.cat([h.repeat(1, N).view(N * N, -1), h.repeat(N, 1)], dim=1).view(N, -1, 2 * self.out_features)
+        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
+        zero_vec = -9e15 * torch.ones_like(e)
+        attention = torch.where(adj > 0, e, zero_vec)
+        attention = F.softmax(attention, dim=1)
+        attention = F.dropout(attention, self.dropout, training=self.training)
+        h_prime = torch.matmul(attention, h)
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+
+
+class GAT(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout, alpha, num_head):
+        super(GAT, self).__init__()
+        self.dropout = dropout
+        self.attentions = [GraphAttentionLayer(input_dim, hidden_dim, dropout=dropout, alpha=alpha, concat=True) for i
+                           in range(num_head)]
+        self.out_att = GraphAttentionLayer(hidden_dim * num_head, output_dim, dropout=dropout, alpha=alpha,
+                                           concat=False)
+
+    def forward(self, x, adj):
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.elu(self.out_att(x, adj))
+        return F.log_softmax(x, dim=1)
